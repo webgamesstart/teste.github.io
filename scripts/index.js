46422bbaf5bdffd8a4a83139a2982940d486fa11
Carlos Timoteo Arruda <carlos.t.arruda@gmail.com>
2020-07-15 09:56:27 -0300

    Teste 2

--- scripts/index.js
+++ scripts/index.js
@@ -11,17 +11,11 @@
 	imgDois.src = './images/dois.jpg';
 	imgTres.src = './images/tres.png';
 
-	function teste(msg){
-		return div.textContent += msg + '<br>';
+	function teste(){
+		return div.textContent = 'imagem carregada';
 	};
 
-	imgTres.decode()
-		.then(() => {
-			div.textContent = teste('Imagem TRES carregada');
-		})
-		.catch((encodingError) => {
-			// Do something with the error.
-		})
+	imgTres.addEventListener('onload', teste);
 
 	
 
